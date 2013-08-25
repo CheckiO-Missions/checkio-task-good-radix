5f6b7d43c2af9f5d732839a32ea6d58afa4f48b6
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-08-26 11:13:39 +1200

    Tests

--- verification/tests.py
+++ verification/tests.py
@@ -10,27 +10,24 @@ Each test is dict with
 
 TESTS = {
     "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+        {"input": "18", "answer": 10},
+        {"input": "1010101011", "answer": 2},
+        {"input": "222", "answer": 3},
+        {"input": "A23B", "answer": 14},
+        {"input": "123F", "answer": 22},
+        {"input": "IDDQD", "answer": 0},
+        {"input": "ZZZ", "answer": 36},
+        {"input": "XYZ", "answer": 0}
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+        {"input": "T", "answer": 30},
+        {"input": "BTA1MCNQL", "answer": 32},
+        {"input": "M3", "answer": 26},
+        {"input": "29", "answer": 12},
+        {"input": "HEEL", "answer": 23},
+        {"input": "33112", "answer": 6},
+        {"input": "707776957", "answer": 12},
+        {"input": "2648", "answer": 11},
+        {"input": "94308", "answer": 13},
     ]
 }
